7374f0496fbdbc993497ea8803de9eb86d4ae609
LiperDev <felipe.devsoftwareengineer@gmail.com>
2025-05-19 19:05:33 -0300

    primeiro_commit

---
+++ Projetos/cadastro_python/funcoes.py
@@ -0,0 +1,30 @@
+def exibir_apresentacao():
+    print("Bem-vindo(a) ao Sistema de Cadastro")
+
+def exibir_menu_opcoes():
+    print("\n1- Cadastrar Usuário no Sistema\n"
+          "2- Verificar Cadastro de Usuário\n"
+          "3- Excluir Cadastro de Usuário\n"
+          "4- Exibir Cadastro Completo do Sistema\n"
+          "5- Atualizar Informação de Usuário\n"
+          "6- Sair do Sistema\n")
+
+def apresentacao_setor(opcao):
+    match opcao:
+        case 1: print("\n| Cadastrar Usuários no Sistema |\n")
+        case 2: print("\n| Verificar Cadastro de Usuário |\n")
+        case 3: print("\n| Excluir Cadastro de Usuário |\n")
+        case 4: print("\n| Exibir Cadastro Completo do Sistema |\n")
+        case 5: print("\n| Atualizar Informação de Usuário |\n")
+        case 6: encerrar_aplicativo()
+        case _: print("\nOpção inexistente\n")
+
+def encerrar_aplicativo():
+    print("Encerrando sistema...")
+    print("Encerrado!")
+
+def confirmacao_cadastro():
+    print("\nO usuário foi cadastrado no sistema!\n")
+
+def identifica_duplicata():
+    print("\nUsuário já cadastrado!\n")
---
+++ Projetos/cadastro_python/main.py
@@ -0,0 +1,98 @@
+import json
+import funcoes
+import os
+
+opcao_digitada = -1
+
+CAMINHO_ARQUIVO = "C:\\Users\\Felipe\\Desktop\\Projetos\\cadastro_python\\cadastro_sistema.txt"
+
+if __name__ == "__main__":
+
+    if os.path.exists(CAMINHO_ARQUIVO) and os.path.getsize(CAMINHO_ARQUIVO) > 0:
+        with open(CAMINHO_ARQUIVO, "r", encoding="UTF-8") as arquivo:
+            usuarios_cadastrados_sistema = json.load(arquivo)
+    else:
+        usuarios_cadastrados_sistema = {}
+
+    while opcao_digitada != 6:
+
+        funcoes.exibir_apresentacao()
+        funcoes.exibir_menu_opcoes()
+        opcao_digitada = int(input("Digite uma opção: "))
+
+        match opcao_digitada:
+            case 1:
+                funcoes.apresentacao_setor(1)
+
+                nome_digitado = input("Digite o nome completo do usuário: ").strip()
+
+                if nome_digitado in usuarios_cadastrados_sistema:
+                    funcoes.identifica_duplicata()
+                else:
+                    email_digitado = input("Digite o email do usuário: ")
+                    celular_digitado = input("Digite o celular do usuário: ")
+                    profissao_digitada = input("Digite a profissão do usuário: ")
+
+                    usuarios_cadastrados_sistema[nome_digitado] = {
+                        "Nome": nome_digitado,
+                        "Email": email_digitado,
+                        "Celular": celular_digitado,
+                        "Profissão": profissao_digitada
+                    }
+
+                    funcoes.confirmacao_cadastro()
+
+                    with open(CAMINHO_ARQUIVO, "w", encoding="UTF-8") as arquivo:
+                        json_string = json.dumps(usuarios_cadastrados_sistema, indent=4, ensure_ascii=False)
+                        arquivo.write(json_string)
+
+            case 2:
+                funcoes.apresentacao_setor(2)
+
+                consulta_nome = input("Digite o nome do usuário: ").strip()
+
+                if consulta_nome in usuarios_cadastrados_sistema:
+                    print(f"{consulta_nome} encontrado no sistema de cadastro!")
+                    info = usuarios_cadastrados_sistema[consulta_nome]
+                    print(f"Email: {info['Email']}")
+                    print(f"Celular: {info['Celular']}")
+                    print(f"Profissão: {info['Profissão']}")
+                else:
+                    print(f"{consulta_nome}, não encontrado no sistema!")
+
+            case 3:
+                funcoes.apresentacao_setor(3)
+
+                consulta_usuario = input("Digite o nome do usuário: ").strip()
+
+                if consulta_usuario in usuarios_cadastrados_sistema:
+                    print(f"{consulta_usuario} encontrado no sistema de cadastro!")
+
+                    procedimento = input("(S- sim ou N- não) Você deseja excluir o cadastro? ").strip().upper()
+
+                    match procedimento:
+                        case "S":
+                            print(f"{consulta_usuario}, está sendo deletado do sistema...")
+                            usuarios_cadastrados_sistema.pop(consulta_usuario)
+
+                            with open(CAMINHO_ARQUIVO, "w", encoding="UTF-8") as arquivo:
+                                json_string = json.dumps(usuarios_cadastrados_sistema, indent=4, ensure_ascii=False)
+                                arquivo.write(json_string)
+
+                        case "N":
+                            print("Retornando ao menu...")
+
+                        case _:
+                            print(f"{procedimento}, é uma opção inválida!")
+
+                else:
+                    print(f"{consulta_usuario}, não encontrado no sistema!")
+
+            case 4:
+                funcoes.apresentacao_setor(4)
+
+            case 5:
+                funcoes.apresentacao_setor(5)
+
+            case 6:
+                funcoes.apresentacao_setor(6)
